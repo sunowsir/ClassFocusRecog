e7f17380671c505d27c108ef3d89ae84a30c2ec4
sunsir-coder <sunow.wang@gmail.com>
2023-03-13 21:13:44 +0800

    [modify] tidy code

---
+++ src/ssi_expression_recognition.cpp
@@ -0,0 +1,16 @@
+/*
+	* File     : ssi_expression_recognition.cpp
+	* Author   : sunowsir
+	* Mail     : sunowsir@163.com
+	* Github   : github.com/sunowsir
+	* Creation : 2023年03月13日 星期一 21时11分41秒
+*/
+
+#include "ssi_expression_recognition.h"
+
+SSI_Expression_Recognition::SSI_Expression_Recognition() {
+}
+
+SSI_Expression_Recognition::~SSI_Expression_Recognition() {
+}
+
---
+++ src/ssi_expression_recognition.h
@@ -0,0 +1,20 @@
+/*
+	* File     : ssi_expression_recognition.h
+	* Author   : sunowsir
+	* Mail     : sunowsir@163.com
+	* Github   : github.com/sunowsir
+	* Creation : 2023年03月13日 星期一 21时10分42秒
+*/
+
+#ifndef _SSI_EXPRESSION_RECOGNITION_H
+#define _SSI_EXPRESSION_RECOGNITION_H
+
+class SSI_Expression_Recognition {
+private: 
+
+public:
+    SSI_Expression_Recognition();
+    ~SSI_Expression_Recognition();
+};
+
+#endif
